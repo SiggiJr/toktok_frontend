714720eda9dfa7f88fc9d3fd742e3cb423bc7858
SiggiJr <fsiegert1@gmail.com>
2023-10-17 15:11:04 +0200

    Login erstellt und encrypt

--- src/utils/fetches/loginFetch.js
+++ src/utils/fetches/loginFetch.js
@@ -1,6 +1,7 @@
 export const loginFetch = async (event, navigate) => {
   const form = new FormData(event.target)
   const response = fetch(`${import.meta.env.VITE_BACKEND_URL}/login`, {
+    credentials: 'include',
     method: 'POST',
     body: form,
   })
