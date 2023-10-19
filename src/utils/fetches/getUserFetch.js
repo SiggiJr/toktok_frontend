766d39891e7239b7ef7a730a9fd514dd860f82a1
S4nt1nO <Gianluca1996@gmx.de>
2023-10-19 11:32:19 +0200

    user fetch

--- src/utils/fetches/getUserFetch.js
+++ src/utils/fetches/getUserFetch.js
@@ -1,5 +1,7 @@
 export const getUser = async () => {
-  const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/user/userdata`)
+  const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/user/userdata`, {
+    credentials: 'include',
+  })
   if (response.ok) {
     const userData = await response.json()
     console.log(userData)
