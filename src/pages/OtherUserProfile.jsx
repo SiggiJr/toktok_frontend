1441ad91bae4fb3d6c514e0a2786484c6b1a88fc
S4nt1nO <Gianluca1996@gmx.de>
2023-10-23 09:45:41 +0200

    get user profile fetch

--- src/pages/OtherUserProfile.jsx
+++ src/pages/OtherUserProfile.jsx
@@ -1,7 +1,15 @@
 import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { getUserProfile } from '../utils/fetches/getUserFetch.js'
 
 function OtherUserProfile() {
   const userId = useParams()
+  const [user, setUser] = useState([])
+
+  useEffect(() => {
+    getUserProfile(userId, setUser)
+    console.log(user)
+  }, [])
   return (
     <div>
       <h1>other users </h1>
