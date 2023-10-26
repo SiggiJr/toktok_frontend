4d1876ec1e7200bd525993f9cb0035dc4c321b39
Hectorcgn <schneiderhector@googlemail.com>
2023-10-26 09:09:57 +0200

    reply page style

--- src/components/ReplyItem.jsx
+++ src/components/ReplyItem.jsx
@@ -10,11 +10,18 @@ export default function ReplyItem({ reply, userId, setReload }) {
   }, [])
 
   return (
-    <div>
-      <img className="w-[48px] h-[48px] object-cover rounded-full " src={user.profile_image_url} alt="" />
-      <h2>{reply.nickname}</h2>
-      <p>{reply.reply}</p>
-      <p>{reply.timestamp}</p>
+    <div className="flex flex-col gap-2 p-6">
+      <div className="flex gap-2 items-center ">
+        <img className="w-[48px] h-[48px] object-cover rounded-full " src={user.profile_image_url} alt="" />
+        <div className="flex gap-2 items-baseline">
+          <h2 className="text-xl">{reply.nickname}</h2>
+          <p className="text-[16px] text-[#616161]"> {reply.timestamp.substring(0, 10)}</p>
+          <p className="text-[16px] text-[#616161]">{reply.timestamp.substring(11, 16)}</p>
+        </div>
+      </div>
+      <div className=" border-gray-200 border-b-[1px] pb-2">
+        <p>{reply.reply}</p>
+      </div>
     </div>
   )
 }
